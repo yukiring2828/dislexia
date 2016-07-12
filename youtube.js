31f2a6cf9bc0d1576400b4bee78912e1cca99b84
Futaba Otake <Otake@Futaba-no-MacBook-Air.local>
2016-07-12 15:27:13 +0900

    vs_value

--- youtube.js
+++ youtube.js
@@ -94,6 +94,8 @@
     function setControl(){
       document.getElementById('offset').value  = offset;
       document.getElementById('offset_value').value  = offset;
+      document.getElementById('vsset').value  = offset;//追加
+      document.getElementById('vs_value').value  = offset;//追加
       document.getElementById('ss').value  = LOWRATE;
       document.getElementById('ss_value').value  = LOWRATE;
       document.getElementById('ms').value  = HIGHRATE;
